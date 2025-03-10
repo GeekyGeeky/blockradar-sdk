dd2b615e142dc4196aac565262309b39a8a8d42d
GeekyGeeky <therealolushola@gmail.com>
2025-03-10 14:39:59 +0100

    test: added wallet test file

---
+++ test/wallet.test.ts
@@ -0,0 +1,15 @@
+import BlockRadar, {WalletWithdrawal} from "../src";
+
+const sdk = new BlockRadar(process.env.BLOCKRADAR_API_KEY!);
+
+describe("Wallet Module", () => {
+  //   let sdk: BlockRadar;
+  //   beforeAll(() => {
+  //     sdk = new BlockRadar(process.env.BLOCKRADAR_API_KEY!);
+  //   });
+
+  it("should get wallet", async () => {
+    const wallet = await sdk.wallet.getWallet(process.env.DEFAULT_WALLET_ID);
+    expect(wallet).toBeDefined();
+  });
+});
