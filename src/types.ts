00995dfdf6489effa257d4929b68f5810c85202f
GeekyGeeky <therealolushola@gmail.com>
2025-03-14 06:04:44 +0100

    refactor: updated types export for all modules

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
-// types.ts
+// export all types to have a single source of all types
+
 export * from "./modules/wallet/interface";
 export * from "./modules/asset/interface";
+export * from "./modules/address/interface";
+export * from "./modules/transaction/interface";
+export * from "./modules/aml/interface";
+export * from "./modules/auto-settlement/interface";
+export * from "./modules/asset-recovery/interface";
+export * from "./modules/payment-link/interface";
+export * from "./modules/smart-contract/interface";
+export * from "./modules/misc/interface";
