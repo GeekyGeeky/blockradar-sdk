02b1a4a407ebac5a50ba6bc56db8d33aa72d0738
GeekyGeeky <therealolushola@gmail.com>
2025-03-15 01:24:27 +0100

    update: added smartcontractparams and response to core interface

--- src/core/interface.ts
+++ src/core/interface.ts
@@ -2,3 +2,19 @@ export interface BaseResponse {
   message: string;
   statusCode: number;
 }
+
+
+export interface SmartContractParams {
+  walletId?: string;
+  address: string;
+  method: string;
+  parameters: string;
+  abi: string;
+}
+
+export interface SmartContractNetworkFeeResponse extends BaseResponse {
+  data: {
+    balance: true;
+    fee: true;
+  };
+}
