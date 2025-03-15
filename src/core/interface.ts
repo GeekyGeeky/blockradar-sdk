f3943cd026a31189bd5f4d288df057076c2025b4
GeekyGeeky <therealolushola@gmail.com>
2025-03-15 03:24:34 +0100

    feat: added `Blockchain` , `Asset` & `Balance` interfaces * added `BalanceResponse` & `BalancesResponse` responses

--- src/core/interface.ts
+++ src/core/interface.ts
@@ -3,6 +3,55 @@ export interface BaseResponse {
   statusCode: number;
 }
 
+export interface Blockchain {
+  id: string;
+  name: string;
+  symbol: string;
+  slug: string;
+  derivationPath: string;
+  isEvmCompatible: boolean;
+  isL2: boolean;
+  logoUrl: string;
+  isActive: boolean;
+  tokenStandard: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Asset {
+  id: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  address: string;
+  standard: string | null;
+  isActive: boolean;
+  logoUrl: string;
+  network: string;
+  createdAt: string;
+  updatedAt: string;
+  blockchain: Blockchain;
+}
+
+export interface Balance {
+  balance: string;
+  convertedBalance: string;
+  asset: {
+    id: string;
+    isActive: true;
+    createdAt: string;
+    updatedAt: string;
+    asset: Asset;
+  };
+}
+
+export interface BalanceResponse extends BaseResponse {
+  data: Balance;
+}
+
+export interface BalancesResponse extends BaseResponse {
+  data: Balance[];
+}
 
 export interface SmartContractParams {
   walletId?: string;
